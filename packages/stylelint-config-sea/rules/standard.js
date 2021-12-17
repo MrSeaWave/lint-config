49787eb7834af3bb5189ed8bfa2b6db64993ba54
MrSeaWave <MrDaemon@outlook.com>
2021-12-17 11:30:33 +0800

    fix: update 注释

--- packages/stylelint-config-sea/rules/standard.js
+++ packages/stylelint-config-sea/rules/standard.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     // 禁止未知的 @ 规则。
     'at-rule-no-unknown': null,
-    // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器。过于严格，暂时关闭
+    // 禁止在具有较高优先级的选择器后出现被其覆盖的较低优先级的选择器。（过于严格，暂时关闭）
     'no-descending-specificity': null,
     // 与 prettier 冲突， 要求在声明块的冒号之后必须有换行符或不能有空白符。
     'declaration-colon-newline-after': null,
