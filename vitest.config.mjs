e18eb5acc9b91cee66e8396bd263296c73a63fc4
MrSeaWave <MrDaemon@outlook.com>
2024-08-13 11:08:37 +0800

    chore: modify test coverage exclude

--- vitest.config.mjs
+++ vitest.config.mjs
@@ -1,9 +1,16 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, coverageConfigDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
     coverage: {
       enabled: true,
+      exclude: [
+        ...coverageConfigDefaults.exclude,
+        'packages/**/test?(s)/**',
+        '**/coverage/**',
+        'scripts',
+        'commitlint.config.js',
+      ],
     },
   },
 });
